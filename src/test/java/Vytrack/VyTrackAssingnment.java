9a12e75710bfa7b8f2cc17780606d56b84b2ac95
zibaablet <ziba.ablet@gmail.com>
2021-12-06 14:52:10 -0800

    Version Changed!

--- src/test/java/Vytrack/VyTrackAssingnment.java
+++ src/test/java/Vytrack/VyTrackAssingnment.java
@@ -45,27 +45,48 @@ public class VyTrackAssingnment {
         //finding click button
         WebElement loginButton = driver.findElement(By.xpath("//button[@id='_submit']"));
         loginButton.click();
+        Thread.sleep(3000);
         Assert.assertEquals(driver.getTitle(), "Dashboard");
         WebElement forFleet = driver.findElement(By.xpath("(//i[@class='fa-asterisk menu-icon'])[1]/.."));
         forFleet.click();
         WebElement odometer = driver.findElement(By.xpath("//span[.='Vehicle Odometer']"));
         odometer.click();
-        Thread.sleep(1234);
+        Thread.sleep(3000);
         Assert.assertTrue(driver.getTitle().contains("Odometer"));
         //verify the user is on the grid information page
 
 
-        Thread.sleep(2000);
+        Thread.sleep(3000);
         //verify user can click on create vehicle odometer button
         driver.findElement(By.xpath("//a[@class='btn main-group btn-primary pull-right ']")).click();
 
+        Thread.sleep(3000);
         //verify user can enter information on the create vehicle odometer page
         driver.findElement(By.xpath("//input[@name='custom_entity_type[OdometerValue]']")).sendKeys("15000");
 
         //verify user can click on cancel button
+        Thread.sleep(3000);
         driver.findElement(By.partialLinkText("Cancel")).click();
 
+        //verify user be able to click on one of the odometer information
+        Thread.sleep(3000);
+        WebElement info= driver.findElement(By.xpath("//table//tr[2]"));
+        info.click();
 
+        Thread.sleep(3000);
+        //verify user can deleter one info
+       driver.findElement(By.xpath("//a[@title='Delete Vehicle Odometer']")).click();
+
+        //click on pop up for delete confirmation
+        Thread.sleep(3000);
+        driver.findElement(By.linkText("Yes, Delete")).click();
+
+        Thread.sleep(3000);
+        //confirm message
+        String actual=driver.findElement(By.xpath("//div[.='Vehicle Odometer deleted']")).getText();
+        Assert.assertEquals(actual,"Vehicle Odometer deleted","Delete confirmation failed ");
+
+        //verify user can edit vehicle information
 
     }
 }
